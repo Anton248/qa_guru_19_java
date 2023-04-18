bd91ee18dcdf99c01f248967c4c20aa39164f36a
Anton Nikolski <31551544+Anton248@users.noreply.github.com>
2023-04-18 14:11:39 +0300

    Update ListExample.java

--- src/main/java/ru/nikolski/collections_examples/ListExample.java
+++ src/main/java/ru/nikolski/collections_examples/ListExample.java
@@ -1,4 +1,14 @@
 package ru.nikolski.collections_examples;
 
 public class ListExample {
+  private List<Card> cards;
+  
+  public ListExample(){
+    cards = new ArrayList<Card>();
+  }
+  
+  public void put(Card card) {
+    cards.put(card);
+  }
+  
 }
