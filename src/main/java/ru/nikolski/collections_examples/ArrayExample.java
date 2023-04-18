8c5b4084ee100e6096f01c8cfd374cd619f8e808
Anton Nikolski <31551544+Anton248@users.noreply.github.com>
2023-04-18 12:53:10 +0300

    Update ArrayExample.java

--- src/main/java/ru/nikolski/collections_examples/ArrayExample.java
+++ src/main/java/ru/nikolski/collections_examples/ArrayExample.java
@@ -1,4 +1,21 @@
 package ru.nikolski.collections_examples;
 
 public class ArrayExample {
+  Card[] cards;
+  
+  public ArrayExample(int size) {
+    cards = new Card[size];
+  }
+  
+  public Card getByOwner(String owner) {
+    Card card = null;
+    for (int i=0; i < cards.length; i++) {
+      if (cards[i].getOwner.equeals(owner)) {
+        card = cards[i].getOwner();
+        break;
+      }
+    }
+    return card;
+  }
+  
 }
